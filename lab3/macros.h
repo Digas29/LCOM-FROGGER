923c926e0af5c043062b75a48a639847db6ae7c8
João Guarda <Digas29@users.noreply.github.com>
2014-10-20 09:55:34 +0000

    test_scan

--- lab3/macros.h
+++ lab3/macros.h
@@ -11,7 +11,12 @@
 
 #define BIT(n) (0x01<<(n))
 #define EXIT_BREAK_CODE 0x81
-#define DELAY_US    20000
-#define hookID 1;
-#define IRQ_KBD 1;
+#define DELAY_US 20000
+#define IRQ_KBD 1
+#define OUT_BUF 0x60
+#define INPT_BUF 0x06
+#define STAT_REG 0x64
+#define KBC_CMD_REG 0x64
+#define PAR_ERR BIT(7)
+#define TO_ERR BIT(6)
 #endif
--- lab3/test3.c
+++ lab3/test3.c
@@ -1,8 +1,11 @@
 #include <minix/sysutil.h>
+#include <minix/syslib.h>
+#include <minix/drivers.h>
 #include "test3.h"
 #include "macros.h"
+#include "kbc.h"
 
-tickdelay(micros_to_ticks(DELAY_US);
+static unsigned int hookID = 1;
 
 int subscribe_kbd(void ) {
 	unsigned int bit = hookID; // bit de subscricao
@@ -20,20 +23,31 @@ int unsubscribe_kbd() {
 	return 0;
 }
 
-void kbd_handler() {
+int kbd_handler() {
+	unsigned long code = kbc_read();
+	printf("Makecode: 0x%x", code);
+	printf("Brakecode: 0x%x", code);
+	code = EXIT_BREAK_CODE;
+	if(code == EXIT_BREAK_CODE){
+		return 1;
+	}
+	return 0;
 
 }
 
-int kbd_test_scan(unsigned short ass); {
+int kbd_test_scan(unsigned short ass){
 	 int ipc_status;
 	 message msg;
 	 int request;
 	 int irq_set;
+	 int stop = 0;
+
+	 tickdelay(micros_to_ticks(DELAY_US));
 
 	 irq_set = BIT(subscribe_kbd());
 
 	 if(ass == 0) {
-		 while(1)) {
+		 while(stop = 0) {
 			 request = driver_receive(ANY, &msg, &ipc_status); /* Get a request message. */
 			 if (request != 0 ) {
 				 printf("driver_receive failed with: %d", request);
@@ -43,7 +57,9 @@ int kbd_test_scan(unsigned short ass); {
 				 switch (_ENDPOINT_P(msg.m_source)) {
 				 case HARDWARE: /* hardware interrupt notification */
 					 if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
-
+						 if(kbd_handler() == 1){
+							 stop = 1;
+						 }
 					 }
 					 break;
 				 default:
@@ -54,7 +70,7 @@ int kbd_test_scan(unsigned short ass); {
 				 /* no standard messages expected: do nothing */
 			 }
 		 }
-		 unsubscribe_int();
+		 unsubscribe_kbd();
 		 return 0;
 	 }
 	 return 1;
