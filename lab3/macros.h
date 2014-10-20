8a781a52b1966dd980cdbce27069f9cb964948a2
João Guarda <Digas29@users.noreply.github.com>
2014-10-20 08:36:55 +0000

    test_scan

---
+++ lab3/macros.h
@@ -0,0 +1,17 @@
+#ifndef _MACROS_
+#define _MACROS_
+
+/** @defgroup i8254 i8254
+ * @{
+ *
+ * Constants for programming the i8254 Timer. Needs to be completed.
+ */
+
+/**< @brief clock frequency for timer in PC and AT */
+
+#define BIT(n) (0x01<<(n))
+#define EXIT_BREAK_CODE 0x81
+#define DELAY_US    20000
+#define hookID 1;
+#define IRQ_KBD 1;
+#endif
