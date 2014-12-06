a8b07f96377343b5121b79f740620859273e06a6
up201303463 <up201303463@df20b149-26ae-4d4a-90c8-1668b27fdd3a>
2014-12-06 17:15:31 +0000

    adding files

---
+++ proj/src/MainMenu.c
@@ -0,0 +1,66 @@
+#include "MainMenu.h"
+#include "mouse.h"
+#include "graphics.h"
+
+MainMenu* newMainMenu(){
+	MainMenu* menu = (MainMenu *) malloc(sizeof(MainMenu));
+
+	menu->done = 0;
+	menu->fundo = loadBitmap("/home/proj/res/cenas.bmp");
+	menu->mousePlay = 0;
+	menu->playButton = newRec(300,100,500,200);
+	menu->mouseHighScores = 0;
+	menu->highScoresButton = newRec(300,250,500,350);
+	menu->mouseExit = 0;
+	menu->exitButton = newRec(300,400,500,500);
+
+	menu->action = 0;
+	return menu;
+}
+int mouseInsideRec(Rec* rect){
+	if(getMouse()->x >= rect->xi &&
+			getMouse()->x <= rect->xf &&
+			getMouse()->y >= rect->yi &&
+			getMouse()->y <= rect->yf){
+		return 1;
+	}
+	else
+		return 0;
+}
+void updateMainMenu(MainMenu* menu, unsigned long scanCode){
+	Mouse * m = getMouse();
+	if(m->leftButtonReleased){
+		if(mouseInsideRec(menu->playButton)){
+			menu->mousePlay = 1;
+		}
+		else{
+			menu->mousePlay = 0;
+		}
+		if(mouseInsideRec(menu->highScoresButton)){
+			menu->mouseHighScores = 1;
+		}
+		else{
+			menu->mouseHighScores = 0;
+		}
+		if(mouseInsideRec(menu->exitButton)){
+			menu->mouseExit = 1;
+			menu->done = 1;
+		}
+		else{
+			menu->mouseExit = 0;
+		}
+	}
+}
+void drawMainMenu(MainMenu* menu){
+	drawBitmap(menu->fundo,0,0,ALIGN_LEFT);
+	bufffer_draw_rectangle(300,200,200,50,50);
+	bufffer_draw_rectangle(300,300,200,50,50);
+	bufffer_draw_rectangle(300,400,200,50,50);
+}
+void deleteMainMenu(MainMenu* menu){
+	deleteBitmap(menu->fundo);
+	deleteRec(menu->playButton);
+	deleteRec(menu->highScoresButton);
+	deleteRec(menu->exitButton);
+	free(menu);
+}
